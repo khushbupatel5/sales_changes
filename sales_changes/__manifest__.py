9afae7711cd414b88d9a4774604900f111f00a29
Khushbu vora <142148210+khushbupatel5@users.noreply.github.com>
2024-02-08 10:04:46 +0530

    Add files via upload

---
+++ sales_changes/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'sales_changes',
+    'version' : '17.0.0.1',
+    'summary': 'sales related changes',
+    'sequence': 10,
+    'description': """   """,
+    'category': '',
+    'website': '',
+    'depends': ['base','sale'],
+    'data': ['view/sales_changes_view.xml'],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
